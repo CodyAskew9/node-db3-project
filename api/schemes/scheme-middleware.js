bf26d71dafa1daefd2a5ab12a6ec5c126a23a50d
cody askew <codyaskew9@gmail.com>
2022-03-23 21:39:11 -0600

    first commit

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,18 +1,23 @@
+const Schemes = require('./scheme-model')
 /*
   If `scheme_id` does not exist in the database:
-
   status 404
   {
     "message": "scheme with scheme_id <actual id> not found"
   }
 */
-const checkSchemeId = (req, res, next) => {
-
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const id = req.params.scheme_id
+    const scheme = await Schemes.findById(id)
+    !scheme ? next({ status: 404 , message: `scheme with scheme_id ${id} not found`}) : next() ;
+  } catch (err) {
+    next(err)
+  }
 }
 
 /*
   If `scheme_name` is missing, empty string or not a string:
-
   status 400
   {
     "message": "invalid scheme_name"
@@ -25,7 +30,6 @@ const validateScheme = (req, res, next) => {
 /*
   If `instructions` is missing, empty string or not a string, or
   if `step_number` is not a number or is smaller than one:
-
   status 400
   {
     "message": "invalid step"
